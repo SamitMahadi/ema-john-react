66a68736bd59c5c933a782a2eeace07cdb8c592e
SamitMahadi <samithossainmahadi@gmail.com>
2022-10-16 13:38:27 -0400

    context added

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Signup.css'
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>
-           <h2>This is signup</h2> 
-        </div>
+        
+<div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+               <div className="form-control">
+                 <label htmlFor="email">Email</label>
+                 <input type="email" name='email' required/>
+               </div>
+
+               <div className="form-control">
+                 <label htmlFor="password">Password</label>
+                 <input type="password" name='password' required/>
+               </div>
+               <div className="form-control">
+                 <label htmlFor="confirm">Confrim Password</label>
+                 <input type="password" name='confirm' required/>
+               </div>
+               <input className='btn-submit' type="submit" name="" id="" value='Login' />
+            </form>
+            <p>Already have an account? <Link to='/login'>Login now</Link></p>
+        </div>       
     );
 };
 
