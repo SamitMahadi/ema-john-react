8fdad1e64954f710b7c2a64dfc4e155257bc1c78
SamitMahadi <samithossainmahadi@gmail.com>
2022-10-16 14:52:17 -0400

    auth added

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.css'
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+const [error,setError]=useState(null)
+
+
+const handleSubmit = (event)=>{
+    event.preventDefault()
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    console.log(email,password,confirm);
+
+ if(password <6){
+    setError('password must be 6 characters or more')
+    return
+ }
+
+ if(password!==confirm){
+ setError('Your Password Did not match');
+ return
+ }
+
+}
+
+
+
     return (
         
 <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                <div className="form-control">
                  <label htmlFor="email">Email</label>
                  <input type="email" name='email' required/>
@@ -18,12 +43,13 @@ const Signup = () => {
                  <input type="password" name='password' required/>
                </div>
                <div className="form-control">
-                 <label htmlFor="confirm">Confrim Password</label>
+                 <label htmlFor="confirm">Confirm Password</label>
                  <input type="password" name='confirm' required/>
                </div>
                <input className='btn-submit' type="submit" name="" id="" value='Login' />
             </form>
             <p>Already have an account? <Link to='/login'>Login now</Link></p>
+            <p className='text-error'>{error}</p>
         </div>       
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UserContext from './Components/Contexts/UserContext';
+import UserContext from './Contexts/UserContext'
+;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserContext>
-      <App />
-    </UserContext>
+   <UserContext>
+     <App />
+   </UserContext>
   </React.StrictMode>
 );
 
